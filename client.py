aad561ef6b0eb612ee4191cbbdfc8743be7cbfe1
Rukchad Wongprayoon <contact@biomooping.tk>
2021-08-30 16:37:10 +0700

    Update client.py

--- client.py
+++ client.py
@@ -5,21 +5,21 @@ from sys import exit
 from os import remove
 from os.path import splitext
 while True:
-	a = input("Screenshot (s) or File (f) or Clipboard Text(ct) or Text (t) or Quit (q)\n")
-	if "s" in a or "S" in a:
+	choices = input("Screenshot (s) or File (f) or Clipboard Text(ct) or Text (t) or Quit (q)\n")
+	if "s" in a or "S" in choices:
 		screen = ImageGrab.grabclipboard()
 		screen.save("pic.jpg")
 		print("Your screenshot is at https://content.biomooping.tk/" + post("https://content.biomooping.tk/save",data=b64encode(open("pic.jpg","rb").read()),headers={"extension":splitext("./pic.jpg")[1]}).text)
 		remove("pic.jpg")
-	elif "f" in a or "F" in a:
+	elif "f" in a or "F" in choices:
 		path = input("Your file path\n")
 		print("Your file is at is at https://content.biomooping.tk/" + post("https://content.biomooping.tk/save",data=b64encode(open(path,"rb").read()),headers={"extension":splitext(path)[1]}).text)
-	elif a.startswith("ct") or a.startswith("CT"):
+	elif choices.startswith("ct") or choices.startswith("CT"):
 		from clipboard import paste
 		print("Your file is at is at https://content.biomooping.tk/" + post("https://content.biomooping.tk/save",data=b64encode(paste()),headers={"extension":"txt"}).text)
-	elif a.startswith("T") or a.startswith("t"):
+	elif choices.startswith("T") or choices.startswith("t"):
 		print("Your file is at is at https://content.biomooping.tk/" + post("https://content.biomooping.tk/save",data=b64encode(bytes(input("Text\n\a"),encoding="utf-8")),headers={"extension":"txt"}).text)
-	elif "q" in a or "Q" in a:
+	elif "q" in a or "Q" in choices:
 		exit("See you next time!")
 	else:
 		continue
